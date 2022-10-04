4cf8df6494d4540778b9b5bf885a579a26bf66a9
LarsDerNorweger <93701694+LarsDerNorweger@users.noreply.github.com>
2022-10-04 14:26:43 +0200

    implement first example

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-import serial
 import time
-
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
-
-
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
+from Serial.SerialManager import SerialManager
+import sys
 
 
-while True:
-    num = input("Enter a number: ")  # Taking input from user
-    value = write_read(num)
-    print(value)  # printing the value
+def convertMessage(msg:str):
+  return int(msg[0:len(msg)-2])
+
+
+def t(this,x):
+  msg = convertMessage(x)
+  if msg == 2048:
+    raise Exception("Unkown Command")
+  if msg == 2049:
+    raise Exception("Argument Exception")
+  if msg == 4096:
+    this.sendLine(next(p))
+  if(msg == 1024):
+    this.stop()
+
+
+def setPin(pin,state,delay):
+  yield str(0)
+  yield str(pin)
+  yield str(state)
+  yield str(delay)
+
+p = setPin(15,1,100)
+s = SerialManager('COM10',t)
+
+
+print("wait")
+s.start()
+s.sendLine(next(p))
+time.sleep(5)
+s.stop()
+print("stop")
+time.sleep(1)
+print("end")
