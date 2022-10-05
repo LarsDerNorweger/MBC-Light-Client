c910545ec0914443c2852aced51bc0a7034c80c6
LarsDerNorweger <boettger.colin@gmail.com>
2022-10-05 11:04:29 +0200

    improve errorhandling

--- UI/translation.py
+++ UI/translation.py
@@ -5,12 +5,11 @@
 #
 
 import json
-global lang
+from tkinter import messagebox
 
 class Language:
     lang = 'en'
     langDic = {}
-
 
 def setLanguage(language:str,path:str= None):
     if(language == 'en'):
@@ -20,18 +19,22 @@ def setLanguage(language:str,path:str= None):
     if path == None and language != 'en':
         raise Exception(f'Translation must be given for {language}')
 
-    Language.lang = language
+    try:
+        Language.lang = language
 
-    with open(path) as fs:
-        res = fs.readlines()
-        Language.langDic = json.loads(convertStringArrayToString(res))
+        with open(path) as fs:
+            res = fs.readlines()
+            Language.langDic = json.loads(convertStringArrayToString(res))
+    except:
+        Language.lang = 'en'
+        messagebox.showerror("Language Error", f"The programm cant read the language Profile, the standart language is choosen")
+        
 
 def convertStringArrayToString(arr):
     res = ''
     for i in arr:
         res+=i
     return res
-
 
 def __(key:str):
     if(Language.lang == 'en'):
