4ff8a7c1ab6ea5bc275fd7c67fbf6248d1e9fb0c
LarsDerNorweger <boettger.colin@gmail.com>
2022-10-06 10:41:47 +0200

    rename and refactor

--- UI/MainWindow/master.py
+++ UI/MainWindow/master.py
@@ -8,8 +8,9 @@ from tkinter import *
 from tkinter import messagebox
 
 from  UI.MainWindow.ArduinoInterface import ArduinoInterface
-from  UI.MainWindow.groups import GroupsInterface
-from  UI.MainWindow.tools import Tools
+from  UI.MainWindow.GroupManager import GroupsInterface
+from  UI.MainWindow.ToolChain import Tools
+from UI.MainWindow.GroupSettingsInterface import GroupSettingsInterface
 
 from UI.translation import setLanguage,__
 from UI.UiHelper import grid
@@ -17,6 +18,7 @@ class MainWindow(Frame):
 
   Arduino:ArduinoInterface
   Groups:GroupsInterface
+  GroupSettings:GroupSettingsInterface
 
   def __init__(self,parent):
     super().__init__(parent)
@@ -30,37 +32,46 @@ class MainWindow(Frame):
       setLanguage('de','./de.json')
 
       self.Arduino = grid(ArduinoInterface(self, './UI/Settings/leonardo.json'),2,1)
-
-      self.Tools = grid(Tools(self),1,1,2)
-      
-      self.Tools.onPinDisable = lambda: self.Arduino.markPins(self.Arduino.get_selected_pins(),"red")
-      self.Tools.onClear = self.__unmark_pins
-
-      self.Groups = grid(GroupsInterface(self, 10),2,2)
-      self.Groups.on_click = lambda:self.Arduino.unselect_all() 
-
       self.Arduino.on_click = self.__add_pins_to_group
+
+      self.Tools = grid(Tools(self),1,1,3)
+      self.Tools.on_select_all = self.__select_All
+      self.Tools.on_delete_from_group = self.__unmark_pins
+
+      self.Groups = grid(GroupsInterface(self, 10),2,2,rowspan=2)
+      self.Groups.on_click = self.__change_group
+
+      self.GroupSettings = grid(GroupSettingsInterface(self),2,3)
+      self.GroupSettings.group = self.Groups.selected_group
+
       self.pack()
     except Exception as e:
       messagebox.showerror(__("An error occured"),e)
       self.master.destroy()
       
+  def __select_All(self):
+    self.Arduino.toggle_all(True)
+    self.__add_pins_to_group()
+    pass
+
+  def __change_group(self):
+    self.Arduino.toggle_all(False)
+    self.GroupSettings.group = self.Groups.selected_group
+    self.Groups.update()
+    pass
 
   def __add_pins_to_group(self):
-    pins = self.Arduino.get_selected_pins()
-    group = self.Groups.get_selected_group()
+    pins = self.Arduino.selected_pins
+    group = self.Groups.selected_group
     group.Pins = group.Pins+list(set(pins)-set(group.Pins))
-    print(group.Pins)
     self.Arduino.mark_pins(pins,group.color)
     pass
 
   def __unmark_pins(self):
-    group = self.Groups.get_selected_group()
-    pins = self.Arduino.get_selected_pins()
+    group = self.Groups.selected_group
+    pins = self.Arduino.selected_pins
     group.Pins = list(set(group.Pins)-set(pins))
-    print(group.Pins)
-
     self.Arduino.unmark_pins(pins)
-    self.Arduino.unselect_all()
+    self.Arduino.toggle_all(False)
     pass
 
