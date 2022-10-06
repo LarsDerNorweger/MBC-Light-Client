02aa112eca902f3adca706d6b9b9c32c44125748
LarsDerNorweger <boettger.colin@gmail.com>
2022-10-06 08:55:18 +0200

    add onclick and rename

--- UI/MainWindow/groups.py
+++ UI/MainWindow/groups.py
@@ -11,45 +11,49 @@
 
 from tkinter import *
 
-from UI.UiHelper import WebsaveColorGenerator
+from UI.UiHelper import WebsaveColorGenerator,packDown
 
 class Group:
+  name:str
   Pins = []
-
   btn:Radiobutton
   color:str
 
-class Groups(Frame):
+class GroupsInterface(Frame):
+
+  @property    
+  def on_click(self)->None:
+      return None
+  @on_click.setter
+  def on_click(self,value):
+      self.__on_click = value
 
   def __init__(self,master,GroupCount):
-
     super().__init__(master)
-
+    self.__on_click = None
     self.groups = []
-
     self.colors = WebsaveColorGenerator(102)
     self.ed = IntVar()
     self.__generateGroup(GroupCount)
 
 
+  def __handle_click(self):
+    if callable(self.__on_click):
+      self.__on_click()
+    pass
+
   def __generateGroup(self,GroupCount):
 
     for i in range(GroupCount):
-
       col = next(self.colors)
-
-      rb = Radiobutton(self,text = f"Group {i}",bg = col,variable=self.ed ,value=i)
-
-      rb.pack()
-
       grp = Group()
-      grp.btn = rb
+      grp.btn = packDown(Radiobutton(self,text = f"Group {i}",bg = col,variable=self.ed ,value=i,command=self.__handle_click))
       grp.color = col
       self.groups.append(grp)
       pass
     pass
 
-  def getSelectedGroup(self)->Group:
+  def get_selected_group(self)->Group:
     return self.groups[self.ed.get()]
 
 
