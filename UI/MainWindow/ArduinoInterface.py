f8d7a4d88058d939b41b2f439acfb96c0e890c83
LarsDerNorweger <boettger.colin@gmail.com>
2022-10-06 08:54:45 +0200

    rename

--- UI/MainWindow/ArduinoInterface.py
+++ UI/MainWindow/ArduinoInterface.py
@@ -13,35 +13,35 @@ import json
 class ArduinoInterface(Frame):
 
   @property    
-  def onClick(self)->None:
+  def on_click(self)->None:
       return None
-  @onClick.setter
-  def onClick(self,value):
+  @on_click.setter
+  def on_click(self,value):
       self.__onClick = value
 
   def __init__(self,parent:Frame,path:str):
     super().__init__(parent)
     self.Pins:List[PIN] = []
-    self.loadArduino(path)
-    self.createArduino()
+    self.__load_arduino(path)
+    self.__create_arduino()
     self.__onClick = None
     pass
 
-  def __handleClick(self):
+  def __handle_click(self):
     if callable(self.__onClick):
       self.__onClick()
     pass
 
-  def createArduino(self):
+  def __create_arduino(self):
     for i,pin in enumerate(self.Pins):
       c = int(i%2)
-      cb = Checkbutton(self,text = f'PIN {pin.pinNumber}',variable=pin.var, command=lambda:self.__handleClick())
+      cb = Checkbutton(self,text = f'PIN {pin.pinNumber}',variable=pin.var, command=lambda:self.__handle_click())
       cb.grid(column=c,row= int((i-c)/2),sticky=W)
       pin.cb = cb
       pass
     pass
 
-  def loadArduino(self,path:str):
+  def __load_arduino(self,path:str):
     try:
       with open(path) as fs:
           res = json.loads(" ".join(fs.readlines()))["pins"]
@@ -55,22 +55,28 @@ class ArduinoInterface(Frame):
       pass
     pass
 
-  def getSelectedPins(self):
+  def get_selected_pins(self):
     res = []
     for pin in self.Pins:
       if pin.var.get() == True:
         res.append(pin.pinNumber)
     return res
 
-  def unselectAll(self):
+  def unselect_all(self):
     for pin in self.Pins:
       pin.var.set(False)
     pass
 
-  def markPins(self,pins,color:str):
+  def mark_pins(self,pins,color:str):
     for pin in self.Pins:
       if pin.pinNumber in pins:
         pin.cb.config(bg = color)
+    pass
+
+  def unmark_pins(self,pins):
+    for pin in self.Pins:
+      if pin.pinNumber in pins:
+        pin.cb.config(bg = "white")
     pass
   
 class PIN:
